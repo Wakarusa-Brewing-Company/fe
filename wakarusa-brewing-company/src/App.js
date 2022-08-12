8d2802b2c2a4acb3857556855f76b98597b3fa50
Richard Perry <85124966+rdperry031@users.noreply.github.com>
2022-08-11 21:15:38 -0500

    created page components and set up initial routing

--- wakarusa-brewing-company/src/App.js
+++ wakarusa-brewing-company/src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Outlet} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Cart from './components/Cart';
+import Contact from './components/Contact';
+import NavBar from './components/NavBar';
+import Products from './components/Products';
+import Store from './components/Store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        ugh
-      </header>
+    <div className='App'>
+      <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='about' element={<About/>} />  
+       <Route path='cart' element={<Cart/>}/>
+       <Route path='contact' element={<Contact/>}/>
+       <Route path='products' element={<Products/>}/>
+       <Route path='store' element={<Store/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ wakarusa-brewing-company/src/components/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <div className = 'home'>
+            <h1>Every moment I live is agony</h1>
+            <p>ugh</p>
+        </div>  
+    )
+}
